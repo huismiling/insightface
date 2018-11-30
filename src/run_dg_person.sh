eb36815107b498b12174aed9852c85f49c8b697e
xiaozhang <xiaozhang@deepglint.com>
2018-11-30 09:59:11 +0800

    up

--- src/run_dg_person.sh
+++ src/run_dg_person.sh
@@ -1,13 +1,13 @@
 
-pretrained='--pretrained /data/xgx/person128/base,67'
+pretrained='--pretrained ../models/person-m0-,2'
 pretrained=
 
-CUDA_VISIBLE_DEVICES='2,3' python -u train_softmax.py \
-    --network r50 --loss-type 2 --margin-m 0.35 \
+CUDA_VISIBLE_DEVICES='0,1' python -u train_softmax.py \
+    --network r50 --loss-type 2 --margin-m 0.35 --version-act relu \
     --data-dir ../datasets/person_reid/ \
-    --prefix ../models/person \
+    --prefix ../models/person-m0-\
     --image-size '128,64' \
     --ckpt 2 --emb-size 128 --per-batch-size 256 \
-    --lr 0.1 --lr-steps 5,10,15 \
+    --lr 0.1 --lr-steps 5,10,20,25 \
     $pretrained | tee train.log
 
